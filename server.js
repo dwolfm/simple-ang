43ff7f44c26dcc319c75dd688287dc419d97414b
Duncan Marsh <dwolfm@gmail.com>
2015-06-11 22:28:50 -0700

    setup basic node server

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// import modules
+var express = require('express');
+var app = express();
+
+// setup env variables
+var port = process.env.PORT || 3000;
+
+// setup routes
+var fourOhfourRouter = express.Router();
+require('./routes/404.js')(fourOhfourRouter);
+app.use('*', fourOhfourRouter);
+
+app.listen( port, function(){
+	console.log('running server on port: ' + port);	
+});
